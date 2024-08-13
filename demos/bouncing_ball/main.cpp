8cf3940338c07f1913701623b22d436801878aa4
James Morgan <james.morgan2@insight.com>
2024-08-13 16:49:45 +0800

    Abstracted behind methods

--- demos/bouncing_ball/main.cpp
+++ demos/bouncing_ball/main.cpp
@@ -24,63 +24,24 @@ int main(void)
     SetTargetFPS(60);
 
     // create our ball
-    Vector2 initPos = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
-    Vector2 initVel = {5, 0};
-    Ball ball{initPos, initVel, 20, RED};
+    Ball ball = CreateBall();
 
     // enter the game loop
     while (!WindowShouldClose()) // window close button or ESC key
     {
         // apply forces to the ball
-        ball.velocity.x += GRAVITY_VEC.x * GetFrameTime();
-        ball.velocity.y += GRAVITY_VEC.y * GetFrameTime();
-
-        ball.position.x += ball.velocity.x;
-        ball.position.y -= ball.velocity.y;
+        UpdateBall(ball);
 
         // check for collisions on screen edges
-        if (ball.position.x >= GetScreenWidth() - ball.radius)
-        {
-            // correct position
-            ball.position.x = GetScreenWidth() - ball.radius;
-
-            // invert and reduce velocity by 40%
-            ball.velocity.x *= -0.6f;
-        }
-        else if (ball.position.x <= ball.radius)
-        {
-            // correct position
-            ball.position.x = ball.radius;
-
-            // invert and reduce velocity by 40%
-            ball.velocity.x *= -0.4f;
-        }
-
-        if (ball.position.y >= GetScreenHeight() - ball.radius)
-        {
-            // correct position
-            ball.position.y = GetScreenHeight() - ball.radius;
-
-            // invert and reduce velocity by 10%
-            ball.velocity.y *= -0.9f;
-        }
-        else if (ball.position.y <= ball.radius)
-        {
-            // correct position
-            ball.position.y = ball.radius;
-
-            // invert and reduce velocity by 10%
-            ball.velocity.y *= -0.9f;
-        }
+        CheckBallCollision(ball);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         // Draw a ball
-        DrawCircleV(ball.position, ball.radius, ball.color);
+        DrawBall(ball);
 
         DrawFPS(10, 10);
-
         EndDrawing();
     }
 
